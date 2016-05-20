a523892f2cde18af977adb05cadd972a38ef2ea6
Bryan Cady <sw@listentothewindmedia.com>
2016-05-20 13:11:40 -0400

    Changed logo size

--- functions.php
+++ functions.php
@@ -50,7 +50,7 @@ add_theme_support( 'genesis-footer-widgets', 3 );
 add_filter( 'genesis_seo_title', 'custom_header_inline_logo', 10, 3 );
 function custom_header_inline_logo( $title, $inside, $wrap ) {
 
-	$logo = '<img src="' . get_stylesheet_directory_uri() . '/images/logo.png" alt="' . esc_attr( get_bloginfo( 'name' ) ) . '" title="' . esc_attr( get_bloginfo( 'name' ) ) . '" width="100" height="133" />';
+	$logo = '<img src="' . get_stylesheet_directory_uri() . '/images/logo.png" alt="' . esc_attr( get_bloginfo( 'name' ) ) . '" title="' . esc_attr( get_bloginfo( 'name' ) ) . '" width="200" height="266" />';
 
 	$inside = sprintf( '<a href="%s" title="%s">%s</a>', trailingslashit( home_url() ), esc_attr( get_bloginfo( 'name' ) ), $logo );
 
